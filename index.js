ac9eab0c0b276eec25025d3415be8962f664c571
Nicole Cabral <asalwaysnicole@outlook.com>
2019-10-04 23:21:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,5 +8,7 @@ function updateObjectWithKeyAndValue(recipes, food, ingredient){
 
 function destructivelyUpdateObjectWithKeyAndValue(recipes, food, ingredient) {
   
-  recipes[food] = ingredient
+  recipes[food] = newIngredient
+  
+  
 }
